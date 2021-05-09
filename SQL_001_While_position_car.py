7a19a12cbfed2c40169f3dd482897c889ed4fe2c
Rico Miraceti <elecam@gmail.com>
2021-05-09 21:21:30 +0200

    sql001

---
+++ SQL_001_While_position_car.py
@@ -0,0 +1,17 @@
+#SQL : exemple de boucle while 
+'''
+SET TEXTSIZE 0;  
+-- Create variables for the character string and for the current   
+-- position in the string.  
+DECLARE @position INT, @string CHAR(8);  
+-- Initialize the current position and the string variables.  
+SET @position = 1;  
+SET @string = 'New Moon';  
+WHILE @position <= DATALENGTH(@string)  
+   BEGIN  
+   SELECT ASCII(SUBSTRING(@string, @position, 1)),   
+      CHAR(ASCII(SUBSTRING(@string, @position, 1)))  
+   SET @position = @position + 1  
+   END;  
+GO  
+'''
